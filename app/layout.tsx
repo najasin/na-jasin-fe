76e3cd15ecad90eb710599dfbd268a1f7583e199
shyjnnn <shyjnnn@gmail.com>
2023-08-05 17:08:58 +0900

    chore: next.js 설치

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
