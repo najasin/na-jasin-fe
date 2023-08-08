33d84309baf2f39d271abaf5100db35c7cd01f36
Yejin Shin <81355590+shyjnnn@users.noreply.github.com>
2023-08-08 20:43:52 +0900

    ProgressBar 컴포넌트 및 스토리북 제작 (#30)
    * feat: progressBar 제작
    
    * feat: progressBar story 제작
    
    ---------
    
    Co-authored-by: shyjnnn <shyjnnn@gmail.com>

---
+++ components/progressBar/progressBar.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import ProgressBar from './progressBar'
+
+const progressBar: Meta<typeof ProgressBar> = {
+  component: ProgressBar,
+}
+
+export default progressBar
+type Story = StoryObj<typeof ProgressBar>
+
+export const Default: Story = {
+  args: { currentSteps: '3', totalSteps: ['1', '2', '3', '4', '5'] },
+}
+
+export const Empty: Story = {
+  args: { currentSteps: '3', totalSteps: ['1', '2', '3', '4', '5'] },
+}
+
+export const Filled: Story = {
+  args: { currentSteps: '3', totalSteps: ['1', '2', '3', '4', '5'] },
+}
