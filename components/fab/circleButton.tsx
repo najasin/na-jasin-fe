16cb100400d607a8bf7ab314b25eef59840d6f33
Shin Minkyu <tlsalsrb990713@gmail.com>
2023-08-08 15:18:39 +0900

    feat: CircleButton 구현

---
+++ components/fab/circleButton.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import classNames from 'classnames/bind'
+
+import Image from 'next/image'
+
+import { CircleButtonProps } from '@/components/fab/circleButton.types'
+
+import styles from './circleButton.styles.module.scss'
+
+const cx = classNames.bind(styles)
+
+export default function CircleButton({
+  name,
+  size = 'md',
+  image,
+  text,
+  onClick,
+}: CircleButtonProps) {
+  const imageUrl = `images/${image}.svg`
+
+  return (
+    <button className={cx('circleBtn', size, image, name)} onClick={onClick}>
+      {text && <p>{text}</p>}
+      {image && <Image src={imageUrl} alt={name} width={30} height={30} />}
+    </button>
+  )
+}
