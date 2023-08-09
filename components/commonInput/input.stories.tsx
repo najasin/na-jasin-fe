b2a4d5ebc561cd6f79731678c08c1dbc75686388
shyjnnn <shyjnnn@gmail.com>
2023-08-09 20:18:33 +0900

    feature: input textfeild story 분리

--- components/commonInput/input.stories.tsx
+++ components/commonInput/input.stories.tsx
@@ -1,39 +1,32 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
 import { Input } from './input'
+import { TextField } from './inputTextField.stories'
 
-const meta: Meta<typeof Input.TextField> = {
+const meta: Meta<typeof Input> = {
   title: 'Input',
-  component: Input.TextField,
+  component: Input,
 }
 export default meta
 
 type Story = StoryObj<typeof Input>
 
-export const Empty: Story = {
-  args: {},
-}
-
 export const Small: Story = {
   args: {
-    placeholder: 'small이어요',
     variant: 'small',
-    onChange: () => console.log('small'),
+    children: <Input.TextField {...TextField.args} />,
   },
 }
 
 export const Medium: Story = {
   args: {
-    placeholder: 'medium입니다.',
     variant: 'medium',
-    onChange: () => console.log('medium'),
+    children: <Input.TextField {...TextField.args} />,
   },
 }
-
 export const Large: Story = {
   args: {
-    placeholder: 'large입니다.',
     variant: 'large',
-    onChange: () => console.log('large'),
+    children: <Input.TextField {...TextField.args} />,
   },
 }
