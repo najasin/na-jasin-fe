f7e22141e676614ff087e8725b100b92e76709b8
wooleejaan <wooleejaan@gmail.com>
2023-08-09 16:21:45 +0900

    feat: width, height 의존성 제거

--- components/radarChart/radarChart.tsx
+++ components/radarChart/radarChart.tsx
@@ -3,9 +3,14 @@
 import classNames from 'classnames/bind'
 
 import styles from './radarChart.module.scss'
+import { IRadarChart } from './radarChart.types'
 
 const cx = classNames.bind(styles)
 
-export default function RadarChart() {
-  return <div className={cx('radarChartWrapper')}>radarChart</div>
+export default function RadarChart({ width, height }: IRadarChart) {
+  return (
+    <div style={{ width, height }} className={cx('radarChartWrapper')}>
+      radarChart
+    </div>
+  )
 }
