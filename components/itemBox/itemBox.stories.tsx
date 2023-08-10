abb4bbf54fbcbba78c0a99a17273e3dcfe16f025
Yejin Shin <81355590+shyjnnn@users.noreply.github.com>
2023-08-10 15:15:51 +0900

    공통 컴포넌트 - item box 구현 (#51)
    * feat: itemBox 제작
    
    * feat: itemBox story 제작
    
    * feat: image 영역 추가
    
    * feaet: itembox story에 mock img 추가
    
    ---------
    
    Co-authored-by: shyjnnn <shyjnnn@gmail.com>

---
+++ components/itemBox/itemBox.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import ItemBox from './itemBox'
+
+type Story = StoryObj<typeof ItemBox>
+
+const itemBox: Meta<typeof ItemBox> = {
+  title: 'ItemBox',
+  component: ItemBox,
+}
+
+export default itemBox
+
+export const Default: Story = {
+  args: {
+    imgUrl: 'https://picsum.photos/id/237/200/300',
+  },
+}
