e58c3767360524272893fb4be7c62d34cd8d3800
redcoin96 <redcoin96@naver.com>
2023-08-10 12:13:27 +0900

    feat: commonBtn 스토리 추가

---
+++ components/commonBtn/commonBtn.stories.ts
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import CommonBtn from './commonBtn'
+import { ButtonStyle } from './commonBtn.types'
+
+const meta: Meta<typeof CommonBtn> = {
+  title: 'CommonBtn',
+  component: CommonBtn,
+}
+
+export default meta
+type Story = StoryObj<typeof CommonBtn>
+
+export const Active: Story = {
+  args: {
+    type: 'submit',
+    style: ButtonStyle.ACTIVE,
+    children: '버튼',
+  },
+}
+
+export const DeActive: Story = {
+  args: {
+    type: 'submit',
+    style: ButtonStyle.DEACTIVE,
+    children: '버튼',
+  },
+}
