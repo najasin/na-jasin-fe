92277225eb669383e481e2c69233e22308069401
shyjnnn <shyjnnn@gmail.com>
2023-08-10 15:41:34 +0900

    feat: itemBtn story 제작

---
+++ components/itemBtn/itemBtn.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { ButtonStyle } from '@/components/commonBtn/commonBtn.types'
+
+import ItemBtn from './itemBtn'
+
+const meta: Meta<typeof ItemBtn> = {
+  title: 'ItemBtn/ItemBtn',
+  component: ItemBtn,
+}
+
+export default meta
+type Story = StoryObj<typeof ItemBtn>
+
+export const Active: Story = {
+  args: {
+    type: 'button',
+    style: ButtonStyle.ACTIVE,
+    children: '버튼',
+  },
+}
+
+export const DeActive: Story = {
+  args: {
+    type: 'button',
+    style: ButtonStyle.DEACTIVE,
+    children: '버튼',
+  },
+}
