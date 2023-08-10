28c042636c84426db9f19b58d49756f05b3e8daa
Shin Minkyu <tlsalsrb990713@gmail.com>
2023-08-11 02:47:53 +0900

    feat: Fab story 작성

---
+++ components/fab/fab.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Fab from './fab'
+
+const fab: Meta<typeof Fab> = {
+  title: 'Components/Fab',
+  component: Fab,
+}
+
+export default fab
+type Story = StoryObj<typeof Fab>
+
+export const Default: Story = {
+  args: {},
+}
