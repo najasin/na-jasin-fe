5460822c91ae66b40b7d001f30c94a71364f1b2e
shyjnnn <shyjnnn@gmail.com>
2023-08-11 21:38:37 +0900

    mock api 구현

---
+++ app/api/jff/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from 'next/server'
+
+import itemsData from '@/public/data/inventory.mockdata.json'
+
+export async function GET() {
+  return NextResponse.json({ itemsData })
+}
