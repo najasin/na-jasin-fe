c0d963ebb52b97c1f12723325ec93737907e46d3
Shin Minkyu <tlsalsrb990713@gmail.com>
2023-08-11 20:18:50 +0900

    feat: CharacterModalLayout story 작성

---
+++ components/modalLayout/characterModalLayout.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import CharacterModalLayout from './characterModalLayout'
+
+const characterModal: Meta<typeof CharacterModalLayout> = {
+  title: 'ModalLayout/CharacterModalLayout',
+  component: CharacterModalLayout,
+}
+
+export default characterModal
+type Story = StoryObj<typeof CharacterModalLayout>
+
+export const Default: Story = {
+  args: {
+    title: '자신이 다시 꾸미기',
+    closeBtn: <button>X</button>,
+    character: <div>Character</div>,
+    content: <div>Content</div>,
+  },
+}
