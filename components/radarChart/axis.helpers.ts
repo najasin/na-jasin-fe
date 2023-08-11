63f3c4143a16733f6388ed78d41f8dcf23267701
wooleejaan <wooleejaan@gmail.com>
2023-08-11 18:57:48 +0900

    feat: mapping helpers 추가 구현

---
+++ components/radarChart/axis.helpers.ts
@@ -0,0 +1,47 @@
+const defaultOffsetMap = [
+  { offsetX: 0, offsetY: -30 },
+  { offsetX: -40, offsetY: 0 },
+  { offsetX: -10, offsetY: 45 },
+  { offsetX: 10, offsetY: 45 },
+  { offsetX: 40, offsetY: 0 },
+]
+
+const rotateOffsetMap = [
+  [
+    { offsetX: 0, offsetY: -20 },
+    { offsetX: -30, offsetY: 0 },
+    { offsetX: 0, offsetY: 20 },
+    { offsetX: 0, offsetY: 20 },
+    { offsetX: 20, offsetY: -20 },
+  ],
+  [
+    { offsetX: 50, offsetY: -100 },
+    { offsetX: -10, offsetY: -15 },
+    { offsetX: -40, offsetY: 20 },
+    { offsetX: -30, offsetY: 40 },
+    { offsetX: 15, offsetY: 15 },
+  ],
+  [
+    { offsetX: 0, offsetY: -20 },
+    { offsetX: -30, offsetY: 0 },
+    { offsetX: 0, offsetY: 20 },
+    { offsetX: 0, offsetY: 20 },
+    { offsetX: 20, offsetY: -20 },
+  ],
+  [
+    { offsetX: 0, offsetY: -20 },
+    { offsetX: -30, offsetY: 0 },
+    { offsetX: 0, offsetY: 20 },
+    { offsetX: 0, offsetY: 20 },
+    { offsetX: 20, offsetY: -20 },
+  ],
+  [
+    { offsetX: 0, offsetY: -20 },
+    { offsetX: -30, offsetY: 0 },
+    { offsetX: 0, offsetY: 20 },
+    { offsetX: 0, offsetY: 20 },
+    { offsetX: 20, offsetY: -20 },
+  ],
+]
+
+export { defaultOffsetMap, rotateOffsetMap }
