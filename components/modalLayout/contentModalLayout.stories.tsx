3f526d11178a0f4fcc7d609f3eacfdf1f5cb2836
Shin Minkyu <tlsalsrb990713@gmail.com>
2023-08-11 20:18:34 +0900

    feat: ContentModalLayout story 작성

---
+++ components/modalLayout/contentModalLayout.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import CommonBtn from '../commonBtn/commonBtn'
+import ContentModalLayout from './contentModalLayout'
+
+const contentModal: Meta<typeof ContentModalLayout> = {
+  title: 'ModalLayout/ContentModalLayout',
+  component: ContentModalLayout,
+}
+
+export default contentModal
+type Story = StoryObj<typeof ContentModalLayout>
+
+export const Default: Story = {
+  args: {
+    title: '자신이 다시 꾸미기',
+    closeBtn: <button>X</button>,
+    content: <div>Content</div>,
+    completeBtn: <CommonBtn>완료</CommonBtn>,
+  },
+}
