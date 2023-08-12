7299efc06fdc4e81fdb284983a1ad672169cf114
Shin Minkyu <122428411+Andy-Shin99@users.noreply.github.com>
2023-08-12 21:52:20 +0900

    hotfix: storybook 반응형 config 추가 (#74)

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,22 @@
 import type { Preview } from '@storybook/react'
 
+const customViewports = {
+  Desktop: {
+    name: 'Desktop',
+    styles: {
+      width: '1440px',
+      height: '800px',
+    },
+  },
+  Mobile: {
+    name: 'Mobile',
+    styles: {
+      width: '375px',
+      height: '856px',
+    },
+  },
+}
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -8,6 +25,9 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+    viewport: {
+      viewports: customViewports,
     },
   },
 }
