c2a0a65035e03b1f899ae3131f70c0696ca571a2
shyjnnn <shyjnnn@gmail.com>
2023-08-13 16:19:56 +0900

    feat: inventoryBox click 기능

--- components/inventory/inventoryItemBoxList/inventoryItemBoxList.tsx
+++ components/inventory/inventoryItemBoxList/inventoryItemBoxList.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import classNames from 'classnames/bind'
 
 import ItemBox from '@/components/itemBox/itemBox'
 
 import { Item } from '../inventory.types'
+import styles from './inventoryItemBoxList.module.scss'
 import { InventoryItemBoxListProps } from './inventoryItemBoxList.types'
+
+const cx = classNames.bind(styles)
 
 export function InventoryItemBoxList({
   selectedCategoryItems,
+  onSelectedItem,
 }: InventoryItemBoxListProps) {
   return (
-    <>
+    <div className={cx('boxList')}>
       {selectedCategoryItems.map((item: Item) => (
-        <ItemBox key={item.id} imgUrl={item.showCase} />
+        <ItemBox
+          key={item.id}
+          imgUrl={item.showCase}
+          onSelectedItem={onSelectedItem}
+        />
       ))}
-    </>
+    </div>
   )
 }
--- components/inventory/inventoryItemBoxList/inventoryItemBoxList.types.ts
+++ components/inventory/inventoryItemBoxList/inventoryItemBoxList.types.ts
@@ -1,7 +1,6 @@
 import { Item } from '../inventory.types'
-import { Category } from '../inventoryCategoryBtnList/inventoryCategoryBtnList.types'
 
 export interface InventoryItemBoxListProps {
-  selectedCategory: Category
   selectedCategoryItems: Item[]
+  onSelectedItem: (img: string) => void
 }
