15e26bc93a9aef569d0d7e8b6496e256cb516402
shyjnnn <shyjnnn@gmail.com>
2023-08-13 18:34:26 +0900

    feat: myManual Atom 제작

---
+++ components/makeMyManual/makeMyManual.atom.tsx
@@ -0,0 +1,28 @@
+import { atom } from 'recoil'
+
+import { Category } from '../inventory/inventoryCategoryBtnList/inventoryCategoryBtnList.types'
+
+export const selectedCategoryState = atom<Category>({
+  key: 'selectedCategoryState',
+  default: 'face',
+})
+
+export const selectedFaceItemState = atom<string>({
+  key: 'selectedFaceItemState',
+  default: '',
+})
+
+export const selectedBodyItemState = atom<string>({
+  key: 'selectedBodyItemState',
+  default: '',
+})
+
+export const selectedExpressionItemState = atom<string>({
+  key: 'selectedExpressionItemState',
+  default: '',
+})
+
+export const selectedSetState = atom<string>({
+  key: 'selectedSetState',
+  default: '',
+})
