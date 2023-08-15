59fc285f4b80034b1ffbc11c608e1786cebc1c99
Shin Minkyu <122428411+Andy-Shin99@users.noreply.github.com>
2023-08-15 22:12:17 +0900

    MyDescriptionCard 수정 hotfix (#109)
    * feat: MyDescriptionCard props 정의
    
    * fix: MyDecriptionCard 수정 반영
    
    * fix: MyDecriptionCard 스타일  수정 반영

--- components/descriptionCard/myDescriptionCard.tsx
+++ components/descriptionCard/myDescriptionCard.tsx
@@ -1,13 +1,31 @@
 import classNames from 'classnames/bind'
 
+import { Input } from '@/components/commonInput/input'
+
 import styles from './myDescriptionCard.module.scss'
+import { IMyDescriptionCardProps } from './myDescriptionCard.types'
 
 const cx = classNames.bind(styles)
 
-export default function MyDescriptionCard({ answer }: { answer: string }) {
+export default function MyDescriptionCard({
+  question,
+  answer,
+  defaultValue,
+}: IMyDescriptionCardProps) {
   return (
     <div className={cx('card')}>
-      <p className={cx('answer')}>{answer}</p>
+      {question.question.split('---')[0]}
+
+      {answer ? (
+        <span className={cx('answer')}>{answer}</span>
+      ) : (
+        <div className={cx('manualInput')}>
+          <Input variant="small">
+            <Input.TextField defaultValue={defaultValue} />
+          </Input>
+        </div>
+      )}
+      {question.question.split('---')[1]}
     </div>
   )
 }
---
+++ components/descriptionCard/myDescriptionCard.types.ts
@@ -0,0 +1,5 @@
+export interface IMyDescriptionCardProps {
+  question: { id: string; question: string }
+  answer?: string
+  defaultValue?: string
+}
