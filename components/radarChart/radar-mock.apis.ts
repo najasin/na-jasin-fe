1b97b5092d59028692d3851cda5996409d98a659
wooleejaan <wooleejaan@gmail.com>
2023-08-17 22:53:21 +0900

    chore: mock-radar 제거

--- components/radarChart/radar-mock.apis.ts
+++
@@ -1,9 +0,0 @@
-import { getRequest } from '@/api/axios/common.apis'
-
-const getRadarData = async () => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const response = await getRequest<any>('/api/mock-radar')
-  return response.data
-}
-
-export { getRadarData }
