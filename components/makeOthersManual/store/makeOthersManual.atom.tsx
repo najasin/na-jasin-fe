4315e58956ed0aa84af249de6caecc3816609b49
redcoin96 <redcoin96@naver.com>
2023-08-18 08:52:23 +0900

    feat: 타적나사 구현

--- components/makeOthersManual/store/makeOthersManual.atom.tsx
+++ components/makeOthersManual/store/makeOthersManual.atom.tsx
@@ -1,11 +1,15 @@
+'use client'
+
 import { atom } from 'recoil'
+
+import { TrimmedDataProps } from '@/components/radarChart/radarChart.types'
 
 export const nicknameState = atom({
   key: 'nicknameState',
   default: '',
 })
 
-export const statsGraphValueState = atom({
+export const statsGraphValueState = atom<TrimmedDataProps>({
   key: 'statsGraphValue',
-  default: '',
+  default: {},
 })
