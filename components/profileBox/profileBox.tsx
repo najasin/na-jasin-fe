86b4520fb42f7012942b0404ae1a8e5ae78538b4
Shin Minkyu <tlsalsrb990713@gmail.com>
2023-08-18 02:47:08 +0900

    feat: 캐릭터 수정 모달 일부 구현

--- components/profileBox/profileBox.tsx
+++ components/profileBox/profileBox.tsx
@@ -1,43 +1,65 @@
+import { useState } from 'react'
+
 import classNames from 'classnames/bind'
 
 import CharacterBox from '@/components/characterBox/characterBox'
+import EditBtn from '@/components/editBtn/editBtn'
+import FormBox from '@/components/formBox/formBox'
+import CloseButton from '@/components/manualBox/closeButton'
+import CharacterModalLayout from '@/components/modalLayout/characterModalLayout'
+import RadarChartContainer from '@/components/radarChart/radarChartContainer'
 
-import EditBtn from '../editBtn/editBtn'
-import RadarChartContainer from '../radarChart/radarChartContainer'
 import styles from './profileBox.module.scss'
+import { IProfileBoxProps } from './profileBox.types'
 
 const cx = classNames.bind(styles)
 
 export default function ProfileBox({
   data,
-}: {
-  data: {
-    itemsData: {
-      baseImage: string
-      selectedItems: { face: string; body: string; expression: string }
-    }
+  myKeywordPercents,
+  othersKeywordPercents,
+}: IProfileBoxProps) {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+
+  const handleClickModalOpen = () => {
+    setIsModalOpen(true)
   }
-}) {
+
+  const handleClickModalClose = () => {
+    setIsModalOpen(false)
+  }
+
   return (
-    <div className={cx('profileBox')}>
-      <div className={cx('characterBox')}>
-        <CharacterBox
-          baseImage={data?.itemsData?.baseImage}
-          selectedItems={data?.itemsData?.selectedItems}
-          editBtn={<EditBtn />}
-        />
+    <>
+      <div className={cx('profileBox')}>
+        <div className={cx('characterBox')}>
+          <CharacterBox
+            baseImage={data?.itemsData?.baseImage}
+            selectedItems={data?.itemsData?.selectedItems}
+            editBtn={<EditBtn onClick={handleClickModalOpen} />}
+          />
+        </div>
+        <div className={cx('chartBox')}>
+          <RadarChartContainer
+            radarType="MY"
+            originKeywordPercents={myKeywordPercents}
+            otherKeywordPercents={othersKeywordPercents}
+            frameSize={340}
+            radarSize={200}
+            framePadding={140}
+            hasOthers={true}
+          />
+        </div>
       </div>
-      <div className={cx('chartBox')}>
-        <RadarChartContainer
-          radarType="MY"
-          originKeywordPercents={{ a: 1, b: 2, c: 3, d: 4, e: 5 }}
-          otherKeywordPercents={{ a: 5, b: 4, c: 3, d: 2, e: 1 }}
-          frameSize={340}
-          radarSize={200}
-          framePadding={140}
-          hasOthers={true}
-        />
-      </div>
-    </div>
+      {isModalOpen && (
+        <CharacterModalLayout
+          title="자시니 다시 꾸미기"
+          closeBtn={<CloseButton onClickModalClose={handleClickModalClose} />}
+          character={<CharacterBox baseImage={data?.itemsData?.baseImage} />}
+        >
+          <FormBox title="나를 꾸며주세요"></FormBox>
+        </CharacterModalLayout>
+      )}
+    </>
   )
 }
