132b23a1761f32ee5b876214614c8c8f9759b448
wooleejaan <wooleejaan@gmail.com>
2023-08-17 09:36:20 +0900

    refactor: Intro와 Feature 사이 여백 추가

--- app/(main)/our-story/page.tsx
+++ app/(main)/our-story/page.tsx
@@ -12,6 +12,7 @@ export default function OurStoryPage() {
   return (
     <div className={cx('pageWrapper')}>
       <OurIntro />
+      <MockHeight />
       <OurFeature />
       <MockHeight />
     </div>
