c76f7c12c5c5a2b5c2ebbc9f04e065f755872ae1
wooleejaan <wooleejaan@gmail.com>
2023-08-17 13:52:46 +0900

    feat: 핸들러 Return type 추가

--- api/axios/requestHandler/auth/kakao.apis.ts
+++ api/axios/requestHandler/auth/kakao.apis.ts
@@ -1,20 +1,12 @@
-import axios from 'axios'
-
 import { getRequest } from '../../common.apis'
 import { AuthResponse } from './auth.types'
 
-const loginInKakao = async () => {
+const loginInKakao = async (): Promise<AuthResponse | Error> => {
   try {
     const response = await getRequest<AuthResponse>('/auth2/authorize/kakao')
 
     return response
   } catch (error) {
-    if (axios.isAxiosError(error)) {
-      if (!error.response?.data.message) {
-        return error as Error
-      }
-      return error.response?.data.message
-    }
     return error as Error
   }
 }
--- api/axios/requestHandler/auth/logout.apis.ts
+++ api/axios/requestHandler/auth/logout.apis.ts
@@ -1,20 +1,12 @@
-import axios from 'axios'
-
 import { postRequest } from '../../common.apis'
 import { LogoutResponse } from './auth.types'
 
-const logout = async () => {
+const logout = async (): Promise<LogoutResponse | Error> => {
   try {
     const response = await postRequest<LogoutResponse>('/auth2/logout')
 
     return response
   } catch (error) {
-    if (axios.isAxiosError(error)) {
-      if (!error.response?.data.message) {
-        return error as Error
-      }
-      return error.response?.data.message
-    }
     return error as Error
   }
 }
