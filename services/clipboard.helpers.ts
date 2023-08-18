2eed3e1d0ddabb35624cbe887a67cb27166a9242
wooleejaan <wooleejaan@gmail.com>
2023-08-18 21:52:42 +0900

    refactor: clipboard, facebook shareUrl 파라미터 추가

--- services/clipboard.helpers.ts
+++ services/clipboard.helpers.ts
@@ -1,11 +1,15 @@
 /**
- *
+ * @param shareUrl 공유할 url (타적나사 + userId 조합으로 된 링크)
  * @param handler state로 ui 변경할 경우 handler를 넘겨주면 됩니다.
  * @example hanlder를 넘겨줄 수 있으므로 () => handleClickCopyClipboard 로 사용해야 합니다.
  */
-const handleClickCopyClipboard = async (handler?: () => void) => {
+const handleClickCopyClipboard = async (
+  shareUrl: string,
+  handler?: () => void,
+) => {
   if (navigator.clipboard && window.isSecureContext) {
-    await navigator.clipboard.writeText(window.location.href)
+    // await navigator.clipboard.writeText(window.location.href)
+    await navigator.clipboard.writeText(shareUrl)
   }
 
   if (handler) {
