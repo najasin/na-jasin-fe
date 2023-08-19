1243d0e370a280e341b199a6dab8ce772f55dc93
wooleejaan <wooleejaan@gmail.com>
2023-08-20 01:38:11 +0900

    fix: next config rewrites 제거

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'https://api.na-jasin.com/:path*',
-      },
-    ]
-  },
   sassOptions: {
     prependData:
       '@use "styles/_utils.scss" as *; @use "styles/_mixin.scss" as *;',
