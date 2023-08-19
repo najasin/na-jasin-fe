7262b1025045d995d7fee06d3ccf005e3b0c736e
shyjnnn <shyjnnn@gmail.com>
2023-08-19 18:56:05 +0900

    refacotore: 폴더 대문자로 변경

--- app/api/jff/characterItems/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server'
-
-import itemsData from '@/public/data/inventory.mockdata.json'
-
-export async function GET() {
-  return NextResponse.json({ ...itemsData })
-}
