f436848b894851d5c659f3693d292ea3bc754323
wooleejaan <wooleejaan@gmail.com>
2023-08-19 13:31:14 +0900

    feat: next.config.js rewrites 수정

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://api.na-jasin.com/:path*',
+      },
+    ]
+  },
   sassOptions: {
     prependData:
       '@use "styles/_utils.scss" as *; @use "styles/_mixin.scss" as *;',
