1ca255d68af06c881385341ea87ba9a9157a94e1
Yong-Woo, Lee <110705019+wooleejaan@users.noreply.github.com>
2023-08-22 16:25:33 +0900

    홈페이지 제외 마우스 유령 추가  (#222)
    * feat: 홈페이지 제외 유령 추가
    
    * feat: 우리들의 이야기 마우스 유령 추가

--- components/ghostCursor/ghostCursorController.tsx
+++ components/ghostCursor/ghostCursorController.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 import GhostCursor from './ghostCursor'
 import { IMouse, IPosition } from './ghostCursor.types'
@@ -16,34 +16,14 @@ export default function GhostCursorController() {
     y: window.innerHeight / 2,
   })
 
-  const [isClicked, setIsClicked] = useState<boolean>(false)
-
-  const handleClick = () => {
-    setIsClicked(true)
-
-    setTimeout(() => {
-      setIsClicked(false)
-    }, 1000)
-  }
-
-  useEffect(() => {
-    window.addEventListener('click', handleClick)
-
-    return () => {
-      window.addEventListener('click', handleClick)
-    }
-  }, [isClicked])
-
   return (
     <>
-      {!isClicked && (
-        <GhostCursor
-          mouse={mouse}
-          setMouse={setMouse}
-          position={position}
-          setPosition={setPosition}
-        />
-      )}
+      <GhostCursor
+        mouse={mouse}
+        setMouse={setMouse}
+        position={position}
+        setPosition={setPosition}
+      />
     </>
   )
 }
