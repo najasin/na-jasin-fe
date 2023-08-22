458bed4daa5461b029d819c800eb5f21064f2d84
wooleejaan <wooleejaan@gmail.com>
2023-08-22 13:11:46 +0900

    fix: 로그아웃 endpoint 수정

--- api/axios/requestHandler/auth/post.apis.ts
+++ api/axios/requestHandler/auth/post.apis.ts
@@ -15,7 +15,7 @@ import { LogoutResponse } from './auth.types'
  * ```
  */
 const logout = async (): Promise<LogoutResponse> => {
-  const response = await postRequest<LogoutResponse>('/api/logout')
+  const response = await postRequest<LogoutResponse>('/api/auth/logout')
 
   return response
 }
