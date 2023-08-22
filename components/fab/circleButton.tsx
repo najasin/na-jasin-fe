5d6cb7b7dd564c065c42c49d71014dc94bd53928
Shin Minkyu <122428411+Andy-Shin99@users.noreply.github.com>
2023-08-22 20:28:45 +0900

    FAB 공유 기능 추가 (#232)
    * refactor: 카카오 공유하기를 위해 id prop type 추가
    
    * feat: 카카오 공유하기 일부 구현(메인 도메인으로 링크되는 문제 해결 필요
    
    * feat: 페이스북, 인스타그램, 링크 공유 기능 추가

--- components/fab/circleButton.tsx
+++ components/fab/circleButton.tsx
@@ -1,16 +1,22 @@
 'use client'
 
+import { useEffect } from 'react'
+
 import classNames from 'classnames/bind'
 
 import Image from 'next/image'
+import { useParams, useSearchParams } from 'next/navigation'
 
 import { CircleButtonProps } from '@/components/fab/circleButton.types'
+
+import createKaKaoShareButton from '@/services/kakao.helpers'
 
 import styles from './circleButton.module.scss'
 
 const cx = classNames.bind(styles)
 
 export default function CircleButton({
+  id,
   name,
   size = 'md',
   image,
@@ -19,10 +25,21 @@ export default function CircleButton({
   transparent,
   onClick,
 }: CircleButtonProps) {
+  const { userType } = useParams()
+  const userId = useSearchParams().get('userId')
+  const shareUrl = `na-jasin.com/${userType}/others-manual?userId=${userId}`
   const imageUrl = `/images/${image}.svg`
+
+  useEffect(() => {
+    if (id === 'kakao-link-btn') {
+      createKaKaoShareButton(shareUrl)
+    }
+  })
 
   return (
     <button
+      id={id}
+      type="button"
       className={cx(
         'circleBtn',
         size,
