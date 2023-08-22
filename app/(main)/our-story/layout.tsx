7fd5673d22ef3de01a71a9e65df24a58bb9fb085
wooleejaan <wooleejaan@gmail.com>
2023-08-22 16:20:54 +0900

    feat: 우리들의 이야기 마우스 유령 추가

---
+++ app/(main)/our-story/layout.tsx
@@ -0,0 +1,21 @@
+import dynamic from 'next/dynamic'
+
+const GhostCursorController = dynamic(
+  () => import('@/components/ghostCursor/ghostCursorController'),
+  {
+    ssr: false,
+  },
+)
+
+export default function OurStoryLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      {children}
+      <GhostCursorController />
+    </>
+  )
+}
