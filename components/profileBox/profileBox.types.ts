f73502709b7be222040c8d0b06721278cb57a716
Shin Minkyu <tlsalsrb990713@gmail.com>
2023-08-22 14:48:57 +0900

    refactor: type 재사용

--- components/profileBox/profileBox.types.ts
+++ components/profileBox/profileBox.types.ts
@@ -1,31 +1,20 @@
+import {
+  CharacterItem,
+  KeywordPercents,
+} from '@/api/axios/requestHandler/mypage/mypage.types'
+
 export interface IProfileBoxProps {
   nickname: string
   data: {
     baseImage: string
     characterItems: {
-      face: {
-        id: number
-        showCase: string
-        layoutCase: string
-      }
-      body: {
-        id: number
-        showCase: string
-        layoutCase: string
-      }
-      expression: {
-        id: number
-        showCase: string
-        layoutCase: string
-      }
-      set: {
-        id: number
-        showCase: string
-        layoutCase: string
-      }
+      face: CharacterItem
+      body: CharacterItem
+      expression: CharacterItem
+      set: CharacterItem
     }
   }
-  myKeywordPercents: { [key: string]: number }
-  othersKeywordPercents: { [key: string]: number }
+  myKeywordPercents: KeywordPercents
+  othersKeywordPercents: KeywordPercents
   isOwner: boolean
 }
