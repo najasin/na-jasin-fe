4dd7e506655dc0a5b22f485b35da14eac3a34f66
wooleejaan <wooleejaan@gmail.com>
2023-08-23 16:43:59 +0900

    refactor: 마우스 애니메이션 분리 (전체 => only mypage)

--- app/(main)/[userType]/layout.tsx
+++ app/(main)/[userType]/layout.tsx
@@ -1,21 +1,7 @@
-import dynamic from 'next/dynamic'
-
-const GhostCursorController = dynamic(
-  () => import('@/components/ghostCursor/ghostCursorController'),
-  {
-    ssr: false,
-  },
-)
-
 export default function UserTypeLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <>
-      {children}
-      <GhostCursorController />
-    </>
-  )
+  return <>{children}</>
 }
