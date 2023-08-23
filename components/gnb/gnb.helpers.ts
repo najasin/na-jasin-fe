9aa4e8a0f3c43438b54c4e9bb65ffaa8d3741df3
wooleejaan <wooleejaan@gmail.com>
2023-08-23 11:24:14 +0900

    feat: Gnb helpers 제작

--- components/gnb/gnb.helpers.ts
+++ components/gnb/gnb.helpers.ts
@@ -1,12 +1,30 @@
-const btnTextHelpers = (path: string) => {
-  if (path === '/') {
-    return '로그인'
+const isLogin = (isLog: boolean) => {
+  if (isLog) {
+    return true
   }
-  if (path === '/jff/others-manual' || path === '/df/others-manual') {
-    return '로그인'
-  }
-
-  return '로그아웃'
+  return false
 }
 
-export { btnTextHelpers }
+const btnTextHelpers = (path: string, isLog: boolean) => {
+  if (path === '/') {
+    return '' // 홈페이지에서 로그인 텍스트 제거
+  }
+
+  if (isLogin(isLog)) {
+    return '로그아웃'
+  }
+  return '로그인'
+}
+
+const userTypeHelpers = (path: string) => {
+  if (path.includes('jff')) {
+    return 'forFun'
+  }
+
+  if (path.includes('df')) {
+    return 'forDev'
+  }
+  return ''
+}
+
+export { btnTextHelpers, userTypeHelpers }
