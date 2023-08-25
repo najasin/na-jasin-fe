b8141e8f761d0f04ee1e8baec79951e7c287f689
redcoin96 <redcoin96@naver.com>
2023-08-25 13:30:51 +0900

    fix: fab 수정

--- components/fab/fab.tsx
+++ components/fab/fab.tsx
@@ -111,11 +111,6 @@ export default function Fab() {
             />
           </div>
         </motion.div>
-        <CopyToast
-          type="success"
-          title="링크 복사 완료!"
-          subtitle="붙여넣어 보세요."
-        />
       </div>
       {isCopied && (
         <CopyToast
