f56312a0c5592c07ed52ebc9334b944fe490e869
wooleejaan <wooleejaan@gmail.com>
2023-08-27 22:56:06 +0900

    fix: Gnb Route 주입

--- components/gnb/gnb.tsx
+++ components/gnb/gnb.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import classNames from 'classnames/bind'
+import { getCookie } from 'cookies-next'
 
 import Image from 'next/image'
+import Link from 'next/link'
 
-// import Link from 'next/link'
 import useBreakpoint from '@/hooks/useBreakpoint.hooks'
 import useScrolledState from '@/hooks/useScrolledState'
 
@@ -16,22 +17,27 @@ export default function Gnb({ children }: { children: React.ReactNode }) {
   const isTablet = useBreakpoint({ query: '(max-width: 1199px)' })
   const scrolled = useScrolledState()
 
+  const act = getCookie('act')
+  const rft = getCookie('rft')
+
+  const href = act || rft ? '/our-story' : '/'
+
   return (
     <nav className={cx('gnbWrapper', { hasBorder: !isTablet }, { scrolled })}>
       <div className={styles.gnbContainer}>
         <div className={styles.left}>
-          {/* <Link href="/"> */}
-          <button type="button">
-            <h1 className={styles.logo}>
-              <Image
-                fill={true}
-                src="/images/logo.png"
-                alt="La Jasin Logo"
-                priority={true}
-              />
-            </h1>
-          </button>
-          {/* </Link> */}
+          <Link href={href}>
+            <button type="button">
+              <h1 className={styles.logo}>
+                <Image
+                  fill={true}
+                  src="/images/logo.png"
+                  alt="La Jasin Logo"
+                  priority={true}
+                />
+              </h1>
+            </button>
+          </Link>
         </div>
         {children}
       </div>
