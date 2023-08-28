ab1499bca36b8ab5492ebf83bdcd7d804e3a1e47
wooleejaan <wooleejaan@gmail.com>
2023-08-28 17:24:10 +0900

    feat: Gnb route 주입

--- components/gnb/gnb.tsx
+++ components/gnb/gnb.tsx
@@ -1,8 +1,13 @@
 'use client'
 
+import { useEffect, useState } from 'react'
+
 import classNames from 'classnames/bind'
+import { getCookie } from 'cookies-next'
 
 import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 import useBreakpoint from '@/hooks/useBreakpoint.hooks'
 import useScrolledState from '@/hooks/useScrolledState'
@@ -15,22 +20,35 @@ export default function Gnb({ children }: { children: React.ReactNode }) {
   const isTablet = useBreakpoint({ query: '(max-width: 1199px)' })
   const scrolled = useScrolledState()
 
+  const pathname = usePathname()
+  const [href, setHref] = useState('/')
+
+  useEffect(() => {
+    if (pathname.includes('our-story')) {
+      setHref('/')
+    } else if (getCookie('act') || getCookie('rft')) {
+      setHref('/our-story')
+    } else {
+      setHref('/')
+    }
+  }, [pathname])
+
   return (
     <nav className={cx('gnbWrapper', { hasBorder: !isTablet }, { scrolled })}>
       <div className={styles.gnbContainer}>
         <div className={styles.left}>
-          {/* <Link href="/"> */}
-          <button type="button" className={styles.button}>
-            <h1 className={styles.logo}>
-              <Image
-                fill={true}
-                src="/images/logo2.png"
-                alt="La Jasin Logo"
-                priority={true}
-              />
-            </h1>
-          </button>
-          {/* </Link> */}
+          <Link href={href}>
+            <button type="button">
+              <h1 className={styles.logo}>
+                <Image
+                  fill={true}
+                  src="/images/logo.png"
+                  alt="La Jasin Logo"
+                  priority={true}
+                />
+              </h1>
+            </button>
+          </Link>
         </div>
         {children}
       </div>
