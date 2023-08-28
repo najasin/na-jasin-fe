bcf5996ef001777eb9a294286e3564ec44579118
wooleejaan <wooleejaan@gmail.com>
2023-08-28 22:46:16 +0900

    feat: footer href 연결

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -12,18 +12,27 @@ export default function Footer() {
           <Link href="/our-story" className={styles.about}>
             우리들의 이야기
           </Link>
-          <Link href="/" className={styles.faq}>
-            FAQ
-          </Link>
         </div>
         <div className={styles.iconContainer}>
-          <Link href="/" className={styles.icon} target="_blank">
+          <Link
+            href="mailto:najasinpro@gmail.com"
+            className={styles.icon}
+            target="_blank"
+          >
             <Image src="/images/email-grey.svg" alt="email" fill={true} />
           </Link>
-          <Link href="/" className={styles.icon} target="_blank">
+          <Link
+            href="https://www.facebook.com/profile.php?id=61550778628237"
+            className={styles.icon}
+            target="_blank"
+          >
             <Image src="/images/facebook-grey.svg" alt="facebook" fill={true} />
           </Link>
-          <Link href="/" className={styles.icon} target="_blank">
+          <Link
+            href="https://www.instagram.com/na_jasin_page/"
+            className={styles.icon}
+            target="_blank"
+          >
             <Image
               src="/images/instagram-grey.svg"
               alt="instagram"
