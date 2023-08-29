86a5d7f7bfc8de71a49d4c0ee29f9e440f7515e4
shyjnnn <shyjnnn@gmail.com>
2023-08-29 11:44:26 +0900

    feat: ga script 설치

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,8 @@ import GnbRight from '@/components/gnb/gnbRight'
 
 import TanstackProvider from '@/api/tanstack/tanstackProvider.context'
 
+import * as gtag from '@/helpers/gtag'
+
 import { META_ROOT } from './_meta'
 
 export const metadata = META_ROOT
@@ -27,22 +29,43 @@ export default function RootLayout({
   const act = cookieStore.get('act')
 
   return (
-    <html lang="ko" className={gmarketSans.className}>
-      <body>
-        <TanstackProvider>
-          <RecoilRootWrapper>
-            <Gnb>
-              <GnbRight isLog={!!(rft || act)} />
-            </Gnb>
-            <main>{children}</main>
-            <Footer />
-          </RecoilRootWrapper>
-        </TanstackProvider>
-        <Script
-          src="https://developers.kakao.com/sdk/js/kakao.js"
-          strategy="beforeInteractive"
-        />
-      </body>
-    </html>
+    <>
+      <Script
+        async={true}
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', ${gtag.GA_TRACKING_ID}, { 
+        page_path: window.location.pathname,
+      });
+    `,
+        }}
+      />
+
+      <html lang="ko" className={gmarketSans.className}>
+        <body>
+          <TanstackProvider>
+            <RecoilRootWrapper>
+              <Gnb>
+                <GnbRight isLog={!!(rft || act)} />
+              </Gnb>
+              <main>{children}</main>
+              <Footer />
+            </RecoilRootWrapper>
+          </TanstackProvider>
+          <Script
+            src="https://developers.kakao.com/sdk/js/kakao.js"
+            strategy="beforeInteractive"
+          />
+        </body>
+      </html>
+    </>
   )
 }
