f70e2e18d96d5a26e5bc91b08e362cfdc222e872
Yong-Woo, Lee <110705019+wooleejaan@users.noreply.github.com>
2023-08-29 14:52:42 +0900

    fix: Link 컴포넌트 => useRouter로 변경 (#342)

--- components/gnb/gnb.tsx
+++ components/gnb/gnb.tsx
@@ -1,13 +1,10 @@
 'use client'
 
-import { useEffect, useState } from 'react'
-
 import classNames from 'classnames/bind'
 import { getCookie } from 'cookies-next'
 
 import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 
 import useBreakpoint from '@/hooks/useBreakpoint.hooks'
 import useScrolledState from '@/hooks/useScrolledState'
@@ -18,37 +15,36 @@ const cx = classNames.bind(styles)
 
 export default function Gnb({ children }: { children: React.ReactNode }) {
   const isTablet = useBreakpoint({ query: '(max-width: 1199px)' })
+  const router = useRouter()
   const scrolled = useScrolledState()
 
   const pathname = usePathname()
-  const [href, setHref] = useState('/')
-
-  useEffect(() => {
-    if (pathname.includes('our-story')) {
-      setHref('/')
-    } else if (getCookie('act') || getCookie('rft')) {
-      setHref('/our-story')
-    } else {
-      setHref('/')
-    }
-  }, [pathname])
 
   return (
     <nav className={cx('gnbWrapper', { hasBorder: !isTablet }, { scrolled })}>
       <div className={styles.gnbContainer}>
         <div className={styles.left}>
-          <Link href={href}>
-            <button type="button">
-              <h1 className={styles.logo}>
-                <Image
-                  fill={true}
-                  src="/images/logo2.png"
-                  alt="La Jasin Logo"
-                  priority={true}
-                />
-              </h1>
-            </button>
-          </Link>
+          <button
+            type="button"
+            onClick={() => {
+              if (pathname.includes('our-story')) {
+                router.push('/')
+              } else if (getCookie('act') || getCookie('rft')) {
+                router.push('/our-story')
+              } else {
+                router.push('/')
+              }
+            }}
+          >
+            <h1 className={styles.logo}>
+              <Image
+                fill={true}
+                src="/images/logo2.png"
+                alt="La Jasin Logo"
+                priority={true}
+              />
+            </h1>
+          </button>
         </div>
         {children}
       </div>
