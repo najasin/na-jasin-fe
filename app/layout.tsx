c47ce3c204a8ae6d14d1519fbfd3b82f2793e0e3
wooleejaan <wooleejaan@gmail.com>
2023-08-31 14:01:06 +0900

    fix: gtag config 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-      gtag('config', ${gtag.GA_TRACKING_ID}, { 
+      gtag('config', '${gtag.GA_TRACKING_ID}', { 
         page_path: window.location.pathname,
       });
     `,
